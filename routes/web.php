592df181119cccabcf8bdbea879631d956b2c9e6
hfulkatan <h.firatulkatan@gmail.com>
2021-11-25 00:50:38 +0300

    updating web.php

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\ArtistController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -21,15 +12,13 @@ Route::get('/', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-Route::get('/artists', [\App\Http\Controllers\ArtistController::class, 'index'])->middleware('auth')->name('artists.index');
-Route::get('/artists/artist/{artist:id}', [\App\Http\Controllers\ArtistController::class, 'show'])->middleware('auth')->name('artists.show');
+Route::group(['prefix' => 'artists', 'middleware' => ['auth']], function(){
+    Route::get('/', [\App\Http\Controllers\ArtistController::class, 'index'])->name('artists.index');
+    Route::get('/artist/{artist:id}', [\App\Http\Controllers\ArtistController::class, 'show'])->name('artists.show');
+});
 
-
-Route::get('/admin/artists/create', [\App\Http\Controllers\Admin\ArtistController::class, 'create'])->middleware(['auth', 'auth.admin'])->name('admin.artists.create');
-Route::post('/admin/artists', [\App\Http\Controllers\Admin\ArtistController::class, 'store'])->middleware(['auth', 'auth.admin'])->name('admin.artists.store');
-Route::get('/admin/artists/{artist:id}/edit', [\App\Http\Controllers\Admin\ArtistController::class, 'edit'])->middleware(['auth', 'auth.admin'])->name('admin.artists.edit');
-Route::patch('/admin/artists/{artist:id}/update', [\App\Http\Controllers\Admin\ArtistController::class, 'update'])->middleware(['auth', 'auth.admin'])->name('admin.artists.update');
-Route::delete('/admin/artists/{artist:id}/destroy', [\App\Http\Controllers\Admin\ArtistController::class, 'destroy'])->middleware(['auth', 'auth.admin'])->name('admin.artists.destroy');
-
+Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'auth.admin']], function() {
+    Route::resource('artists', ArtistController::class)->except(['index', 'show']);
+});
 
 require __DIR__ . '/auth.php';
